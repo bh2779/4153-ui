cdd41587c0a38101c1b20d11f641bf62d4d4f2c2
Benjamin Hong <bhong8989@gmail.com>
2024-11-23 18:51:49 -0500

    removed unnecessary file and comments

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,10 @@
-import React, { useState } from "react";
 import Header from "../components/Header/Header";
 import ReviewForm from "../components/ReviewForm/ReviewForm";
 
 const Home = () => {
-	const [reviews, setReviews] = useState(null);
-
-	// async function handleClick(e) {
-	//     e.preventDefault()
-	//     const response = getDishReviews(1)
-	//     // const json = response.json()
-	//     setReviews(response)
-	// }
-
-	const handleClick = async () => {
-		const response = await fetch("http://54.166.38.176:8000/dishes/1/reviews");
-		const json = await response.json();
-		setReviews(json);
-	};
-
 	return (
 		// <Header />
 		<ReviewForm />
-		// <>
-		// 	<div className="home">
-		// 		<Center>
-		// 			<Stack>
-		// 				<h1 className="home__title">Dining Hall Dish Reviews</h1>
-
-		// 				<TestButton text="Pizza" />
-		// 				{/* {reviews && <p>{JSON.stringify(reviews, null, 2)}</p>} */}
-		// 			</Stack>
-		// 		</Center>
-		// 	</div>
-		// </>
 	);
 };
 
